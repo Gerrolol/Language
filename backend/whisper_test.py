42a8c5ac305e8147225f1508a7f79f28a37ae6bc
unknown <you@example.com>
2025-05-31 19:10:42 +0930

    added rendering files

--- backend/whisper_test.py
+++ backend/whisper_test.py
@@ -131,5 +131,5 @@ def cleanup():
         except:
             pass
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000, debug=False)
